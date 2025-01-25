1a20c472aea6348146c9108497bdd0acfbcf0531
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-01-25 16:52:57 +0100

    Filter available results

--- scripts/main.gd
+++ scripts/main.gd
@@ -1,1 +1,21 @@
 extends Control
+@export var data: GameData
+
+@export var available_results: ItemList
+
+
+func get_related_results(query: Query) -> Array[Result]:
+	var query_tags := query.negative_tags + query.positive_tags
+	return data.results.filter(func(r: Result): return filter_result(r, query_tags))
+	
+func filter_result(r: Result, tags: Array[String]) -> bool:
+	return r.positive_tags.any(func(t: String): return tags.has(t)) or r.negative_tags.any(func(t: String): return tags.has(t))
+
+func _ready() -> void:
+	var human: Human = data.humans.pick_random()
+	var query: Query = human.queries.pick_random()
+	var results := get_related_results(query)
+	
+	available_results.clear()
+	for r in results:
+		available_results.add_item(r.title)
---
+++ scripts/resources/game_data.gd
@@ -0,0 +1,5 @@
+class_name GameData
+extends Resource
+
+@export var humans: Array[Human]
+@export var results: Array[Result]
