05c92fbfba373491bca295eab9df0195e0fa55f4
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-01-25 17:32:00 +0100

    Fill query bar

--- scripts/main.gd
+++ scripts/main.gd
@@ -2,6 +2,10 @@ extends Control
 @export var data: GameData
 
 @export var available_results: ItemList
+@export var search_bar: SearchBar
+
+@export var current_human: Human
+@export var current_query: Query
 
 
 func get_related_results(query: Query) -> Array[Result]:
@@ -11,11 +15,16 @@ func get_related_results(query: Query) -> Array[Result]:
 func filter_result(r: Result, tags: Array[String]) -> bool:
 	return r.positive_tags.any(func(t: String): return tags.has(t)) or r.negative_tags.any(func(t: String): return tags.has(t))
 
-func _ready() -> void:
-	var human: Human = data.humans.pick_random()
-	var query: Query = human.queries.pick_random()
-	var results := get_related_results(query)
+func show_results() -> void:
+	var results := get_related_results(current_query)
 	
 	available_results.clear()
 	for r in results:
 		available_results.add_item(r.title)
+		
+func _ready() -> void:
+	current_human = data.humans.pick_random()
+	current_query = current_human.queries.pick_random()
+	
+	search_bar.show_query(current_query)
+	show_results()
--- scripts/search_bar.gd
+++ scripts/search_bar.gd
@@ -1,10 +1,16 @@
 @tool
+class_name SearchBar
 extends Control
 
 @export var container: HBoxContainer
+@export var query_field: LineEdit
+
 @export var flip: bool = true
 
 func _process(_delta: float) -> void:
-	custom_minimum_size = container.get_rect().size	
+	custom_minimum_size = container.get_rect().size
 	container.pivot_offset = container.get_rect().size / 2
 	container.scale.x = -1 if flip else 1
+
+func show_query(query: Query) -> void:
+	query_field.text = query.text
