5f0f700d816decfc7a73d2f15183770aab357c6e
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-01-25 18:05:01 +0100

    Fix minimum search bar size

--- scripts/search_bar.gd
+++ scripts/search_bar.gd
@@ -8,7 +8,7 @@ extends Control
 @export var flip: bool = true
 
 func _process(_delta: float) -> void:
-	custom_minimum_size = container.get_rect().size
+	custom_minimum_size.y = abs(container.get_rect().size.y)
 	container.pivot_offset = container.get_rect().size / 2
 	container.scale.x = -1 if flip else 1
 
