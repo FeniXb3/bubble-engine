7e7b9cfaa9f4d6f24d1ed5675af701511f666d89
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-01-28 21:42:24 +0100

    [editor] Use callable as metadata for tree items to handle custom editing logic

--- scripts/data_editor.gd
+++ scripts/data_editor.gd
@@ -20,12 +20,7 @@ extends Control
 		discard_button.disabled = save_button.disabled
 var tag_holders_to_update: Array[TreeItem]
 
-const TYPE_COLUMN := 0
 const NAME_COLUMN := 0
-const MOOD_COLUMN := 0
-#const MOOD_ICON_COLUMN := 2
-#const POSITIVE_COLUMN := 3
-#const NEGATIVE_COLUMN := 4
 
 func _ready() -> void:
 	save_dialog.file_selected.connect(save_data)
@@ -35,8 +30,7 @@ func _ready() -> void:
 	DataManager.save_data()
 	DataManager.load_data()
 	game_data = DataManager.data
-	#populate_tree()
-	
+
 func populate_tree() -> void:
 	tree.clear()
 	tag_holders_to_update.clear()
@@ -44,70 +38,51 @@ func populate_tree() -> void:
 	root.set_metadata(0, game_data)
 	
 	print(JSON.stringify(game_data).c_unescape())
-	#tree.set_column_title(TYPE_COLUMN, "Type")
 	tree.set_column_title(NAME_COLUMN, "Name")
-	tree.set_column_title(MOOD_COLUMN, "Mood Value")
-	#tree.set_column_title(MOOD_ICON_COLUMN, "Mood Face")
 	
 	var tags_parent_branch := tree.create_item(root)
-	tags_parent_branch.set_text(TYPE_COLUMN, "Available Tags")
+	tags_parent_branch.set_text(NAME_COLUMN, "Available Tags")
 	tags_parent_branch.set_metadata(0, game_data.tags)
 	
-	for t in game_data.tags:
-		var item := _create_editable_item_with_text(tags_parent_branch, t)
-		item.set_metadata(0, t)
+	for i in game_data.tags.size():
+		var tag_item := _create_editable_item_with_text(tags_parent_branch, game_data.tags[i])
+		tag_item.set_metadata(0, func(item: TreeItem): _update_available_tag(item, i, game_data.tags))
 	
 	var humans_parent_branch := tree.create_item(root)
-	humans_parent_branch.set_text(TYPE_COLUMN, "Humans")
+	humans_parent_branch.set_text(NAME_COLUMN, "Humans")
 	
 	humans_parent_branch.set_metadata(0, game_data.humans)
 	
 	for h in game_data.humans:
 		var human_branch := _create_editable_item_with_text(humans_parent_branch, h.name)
-		human_branch.set_metadata(0, h)
-	
-		#human_branch.set_cell_mode(MOOD_COLUMN, TreeItem.CELL_MODE_CUSTOM)
+		human_branch.set_metadata(0, func(item: TreeItem): h.name = item.get_text(NAME_COLUMN))
 		
 		var mood_parent_branch := tree.create_item(human_branch)
-		mood_parent_branch.set_text(TYPE_COLUMN, "Mood")
-		mood_parent_branch.set_metadata(0, h)
+		mood_parent_branch.set_text(NAME_COLUMN, "Mood")
 		
 		var face_texture = human_visuals.faces.get(h.mood)
 		mood_parent_branch.set_icon(NAME_COLUMN, face_texture)
 		
-		
 		var mood_branch := tree.create_item(mood_parent_branch)
-		#mood_branch.set_text(TYPE_COLUMN, "Mood")
-		mood_branch.set_cell_mode(MOOD_COLUMN, TreeItem.CELL_MODE_RANGE)
-		mood_branch.set_range_config(MOOD_COLUMN, worst_mood, best_mood, 1)
-		mood_branch.set_range(MOOD_COLUMN, h.mood)
-		mood_branch.set_editable(MOOD_COLUMN, true)
-		#mood_branch.set_metadata(0, h.mood)
+		mood_branch.set_cell_mode(NAME_COLUMN, TreeItem.CELL_MODE_RANGE)
+		mood_branch.set_range_config(NAME_COLUMN, worst_mood, best_mood, 1)
+		mood_branch.set_range(NAME_COLUMN, h.mood)
+		mood_branch.set_editable(NAME_COLUMN, true)
 		mood_branch.set_metadata(0, func(item: TreeItem): _update_mood(item, h))
 		
-		
-		
-		#human_branch.set_cell_mode(MOOD_COLUMN, TreeItem.CELL_MODE_RANGE)
-		#human_branch.set_range_config(MOOD_COLUMN, worst_mood, best_mood, 1)
-		#human_branch.set_range(MOOD_COLUMN, h.mood)
-		#human_branch.set_editable(MOOD_COLUMN, true)
-		
-		#human_branch.set_cell_mode(MOOD_ICON_COLUMN, TreeItem.CELL_MODE_ICON)
-		
-		
 		var queries_parent_branch := tree.create_item(human_branch)
-		queries_parent_branch.set_text(TYPE_COLUMN, "Queries")
+		queries_parent_branch.set_text(NAME_COLUMN, "Queries")
 		queries_parent_branch.set_metadata(0, h.queries)
 		
 		for q in h.queries:
 			var query_branch := _create_editable_item_with_text(queries_parent_branch, q.text)
-			query_branch.set_metadata(0, q)
+			query_branch.set_metadata(0, func(item: TreeItem): q.text = item.get_text(NAME_COLUMN))
 			_add_tags(query_branch, q)
 		
 		_add_tags(human_branch, h)
 
 	var results_parent_branch := tree.create_item(root)
-	results_parent_branch.set_text(TYPE_COLUMN, "Results")
+	results_parent_branch.set_text(NAME_COLUMN, "Results")
 	results_parent_branch.set_metadata(0, game_data.results)
 	
 	for r in game_data.results:
@@ -119,7 +94,7 @@ func populate_tree() -> void:
 	root.collapsed = false
 	
 func _update_mood(item: TreeItem, human: Human) -> void:
-	var mood := int(item.get_range(MOOD_COLUMN))
+	var mood := int(item.get_range(NAME_COLUMN))
 	var new_face_texture = human_visuals.faces.get(mood)
 	item.get_parent().set_icon.call_deferred(NAME_COLUMN, new_face_texture)
 	human.mood = mood
@@ -133,63 +108,37 @@ func _add_array_dropdowns(parent_branch: TreeItem, dropdowns_name: String, data:
 	dropdowns_parent_branch.set_text(NAME_COLUMN, dropdowns_name)
 	dropdowns_parent_branch.set_metadata(0, data)
 	
-	for t in data:
+	for i in data.size():
 		var all_tags := ("---," if add_empty_element else "") + ",".join(options)
-		var selected_index := game_data.tags.find(t) + (1 if add_empty_element else 0)
-		var item := _create_editable_dropdown_item(dropdowns_parent_branch, all_tags, selected_index)
-		item.set_metadata(0, t)
+		var selected_index := game_data.tags.find(data[i]) + (1 if add_empty_element else 0)
+		var tag_item := _create_editable_dropdown_item(dropdowns_parent_branch, all_tags, selected_index)
+		tag_item.set_metadata(0, func(item: TreeItem): _update_tag(item, i, data))
 		
-		tag_holders_to_update.append(item)
+		tag_holders_to_update.append(tag_item)
 
 func _on_tree_item_edited() -> void:
 	modified = true
 	var item := tree.get_edited()
-	var column := tree.get_edited_column()
 	var metadata = item.get_metadata(0)
 	
-	if metadata is Human:
-		if column == NAME_COLUMN:
-			metadata.name = item.get_text(column)
-		elif column == MOOD_COLUMN:
-			var mood := int(item.get_range(MOOD_COLUMN))
-			var face_texture = human_visuals.faces.get(mood)
-			item.set_icon.call_deferred(NAME_COLUMN, face_texture)
-			
-			metadata.mood = mood
-	elif metadata is String:
-		var parent_metadata = item.get_parent().get_metadata(0) as Array
-		var index = parent_metadata.find(metadata)
-		if item.get_parent().get_parent().get_metadata(0) is GameData:
-			parent_metadata[index] = item.get_text(0)
-			item.set_metadata(0, item.get_text(0))
-			_update_tags_dropdown()
-		else:
-			var available_tag_index: int = int(item.get_range(0))
-			parent_metadata[index] = game_data.tags[available_tag_index - 1]
-		
-		
-	elif metadata is Result:
-		metadata.title = item.get_text(column)
-	elif metadata is Query:
-		metadata.text = item.get_text(column)
-	elif metadata is Callable:
+	if metadata is Callable:
 		metadata.call(item)
 		
 func _create_editable_item_with_text(parent: TreeItem, text: String, column: int = NAME_COLUMN) -> TreeItem:
-		var item := tree.create_item(parent)
-		item.set_text(column, text)
-		item.set_editable(NAME_COLUMN, true)
-		
-		return item
+	var item := tree.create_item(parent)
+	item.set_text(column, text)
+	item.set_editable(NAME_COLUMN, true)
+	
+	return item
 
 func _create_editable_dropdown_item(parent: TreeItem, text: String, selected_index: int, column: int = NAME_COLUMN) -> TreeItem:
-		var item := tree.create_item(parent)
-		item.set_cell_mode(NAME_COLUMN, TreeItem.CELL_MODE_RANGE)
-		item.set_text(column, text)
-		item.set_range(NAME_COLUMN, selected_index)
-		item.set_editable(NAME_COLUMN, true)
-		
-		return item
+	var item := tree.create_item(parent)
+	item.set_cell_mode(NAME_COLUMN, TreeItem.CELL_MODE_RANGE)
+	item.set_text(column, text)
+	item.set_range(NAME_COLUMN, selected_index)
+	item.set_editable(NAME_COLUMN, true)
+	
+	return item
 
 func _on_save_as_button_pressed() -> void:
 	save_dialog.show()
@@ -201,7 +150,7 @@ func save_data(path: String) -> void:
 
 func _on_load_button_pressed() -> void:
 	load_dialog.show()
-	
+
 func load_data(path: String) -> void:
 	current_path = path
 	DataManager.load_data(path)
@@ -219,9 +168,7 @@ func _on_save_button_pressed() -> void:
 	DataManager.save_data(current_path)
 	modified = false
 
-
 func _on_load_defaults_button_pressed() -> void:
-	#current_path = DataManager.default_path
 	DataManager.load_data()
 	game_data = DataManager.data
 	modified = false
@@ -232,8 +179,13 @@ func _update_tags_dropdown(add_empty_element: bool = true) -> void:
 	for item in tag_holders_to_update:
 		var all_tags := ("---," if add_empty_element else "") + ",".join(options)
 		item.set_text(0, all_tags)
-		var metadata = item.get_metadata(0)
+		item.get_metadata(0).call(item)
+
+func _update_available_tag(item: TreeItem, index: int, tags: Array[String]):
+	tags[index] = item.get_text(0)
+	item.set_metadata(0, item.get_text(0))
+	_update_tags_dropdown()
 		
-		var parent_metadata = item.get_parent().get_metadata(0) as Array
-		var index = parent_metadata.find(metadata)
-		parent_metadata[index] = game_data.tags[int(item.get_range(0)) - 1]
+func _update_tag(item: TreeItem, index: int, tags: Array[String]):
+	var available_tag_index: int = int(item.get_range(0))
+	tags[index] = game_data.tags[available_tag_index - 1]
