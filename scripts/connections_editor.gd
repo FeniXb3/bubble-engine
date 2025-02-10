f3d045f1d3b084750e16a1fb1c5804c267c90636
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-02-10 17:48:38 +0100

    Allow disconnecting

--- scripts/connections_editor.gd
+++ scripts/connections_editor.gd
@@ -37,3 +37,7 @@ func _ready() -> void:
 
 func _on_graph_edit_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	graph.connect_node(from_node, from_port, to_node, to_port)
+
+
+func _on_graph_edit_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
+	graph.disconnect_node(from_node, from_port, to_node, to_port)
