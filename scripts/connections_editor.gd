60924c7d92eecee59865dfc89e44cd3a2cd50c86
Konrad Gadzina <konrad@enklawa.edu.pl>
2025-03-30 09:16:16 +0200

    Add base for filtering automation

--- scripts/connections_editor.gd
+++ scripts/connections_editor.gd
@@ -11,7 +11,6 @@ extends Control
 @export var tags_nodes: Dictionary[String, GraphNode]
 @export var humans_nodes: Dictionary[Human, GraphNode]
 @export var queries_nodes: Dictionary[Query, GraphNode]
-@export var add_real_connections: bool = false
 @export var timer: Timer
 @export var animation_duration: float = 0.25
 
@@ -25,9 +24,10 @@ var bottom_right := Vector2()
 var operations_queue: Array[Callable] = []
 var nodes_to_show: Array[GraphNode] = []
 var selected_node: GraphNode
+var automation: Dictionary = {}
 
-const POSITIVE_PORT = 0
-const NEGATIVE_PORT = 1
+const POSITIVE_PORT = 1
+const NEGATIVE_PORT = 2
 
 const HUMANS_COLUMN = 0
 const QUERIES_COLUMN = 1
@@ -132,7 +132,7 @@ func _show_result(result: Result):
 func add_graph_node(node_scene: PackedScene, node_data, nodes_dictionary: Dictionary, index: int, column: int, text_getter: Callable) -> DataGraphNode:
 	var node := node_scene.instantiate() as DataGraphNode
 	node.setup(index, text_getter.call(), column * columns_separation, initial_nodes_margin, node_data)
-	graph.add_child(node)
+	graph.add_child(node, true)
 	nodes_dictionary.set(node_data, node)
 
 	return node
@@ -144,24 +144,40 @@ func add_nodes() -> void:
 		
 	for i in data.humans.size():
 		var human := data.humans[i]
-		var node := add_graph_node(human_node_scene, human, humans_nodes, i, HUMANS_COLUMN, func(): return human.name)
+		add_graph_node(human_node_scene, human, humans_nodes, i, HUMANS_COLUMN, func(): return human.name)
 		
 		for j in human.queries.size():
 			var query := human.queries[j]
 			add_graph_node(query_node_scene, query, queries_nodes, j, QUERIES_COLUMN, func(): return query.text)
 
-		if add_real_connections:
-			for positive_tag in human.positive_tags:
-				var tag_node := tags_nodes[positive_tag]
-				graph.connect_node(node.name, POSITIVE_PORT, tag_node.name, POSITIVE_PORT)
-
 
 func _on_graph_edit_connection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	graph.connect_node(from_node, from_port, to_node, to_port)
+	var from_data = graph.get_node(NodePath(from_node)).get_meta("data")
+	var to_data = graph.get_node(NodePath(to_node)).get_meta("data")
+	
+	if from_data is Human and to_data is String:
+		var  tags = automation.get_or_add(from_data, {"positive": [], "negative": []})
+		match from_port:
+			POSITIVE_PORT:
+				tags["positive"].append(to_data)
+			NEGATIVE_PORT:
+				tags["negative"].append(to_data)
+	
 
 
 func _on_graph_edit_disconnection_request(from_node: StringName, from_port: int, to_node: StringName, to_port: int) -> void:
 	graph.disconnect_node(from_node, from_port, to_node, to_port)
+	var from_data = graph.get_node(NodePath(from_node)).get_meta("data")
+	var to_data = graph.get_node(NodePath(to_node)).get_meta("data")
+	
+	if from_data is Human and to_data is String:
+		var  tags = automation.get_or_add(from_data, {"positive": [], "negative": []})
+		match from_port:
+			POSITIVE_PORT:
+				tags["positive"].erase(to_data)
+			NEGATIVE_PORT:
+				tags["negative"].erase(to_data)
 
 
 func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
@@ -173,7 +189,6 @@ func _on_visible_on_screen_notifier_2d_screen_entered() -> void:
 	while not nodes_to_show.is_empty():
 		var node := nodes_to_show.pop_front() as GraphNode
 		await _show_node(node).finished
-		
 
 
 func _on_graph_edit_end_node_move() -> void:
